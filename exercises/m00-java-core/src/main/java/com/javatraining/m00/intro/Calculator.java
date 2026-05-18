6f217cd31b3bde8570882c91d3a71579274209a5
Anuj <anujsainirit@gmail.com>
2026-05-18 21:50:50 +0530

    Added Sample test

---
+++ exercises/m00-java-core/src/main/java/com/javatraining/m00/intro/Calculator.java
@@ -0,0 +1,16 @@
+package com.javatraining.m00.intro;
+
+/**
+ * EXERCISE M00-T1
+ * DO NOT change method signatures, class names, or package.
+ */
+
+public class Calculator {
+
+    /**
+     * Return the vehicle's registration number (non-null, non-blank).
+     */
+    public int calculate(int x, int y) {
+        return x+y;
+    }
+}
